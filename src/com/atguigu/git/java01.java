92a06dc7e8064ea0c4274bc9b812fd6058318e0a
ttj <ttj@qq.com>
2024-02-17 20:21:52 +0800

    git java02

--- src/com/atguigu/git/java01.java
+++ src/com/atguigu/git/java01.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class java01 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
