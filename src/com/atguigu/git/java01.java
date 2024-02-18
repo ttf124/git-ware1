b115cfa3fe528c4299f5509b9e26f89a068dd7f4
ttf124 <155542174+ttf124@users.noreply.github.com>
2024-02-18 12:04:38 +0800

    Update java01.java

--- src/com/atguigu/git/java01.java
+++ src/com/atguigu/git/java01.java
@@ -4,5 +4,7 @@ public class java01 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+
+        System.out.println("pull git1");
     }
 }
